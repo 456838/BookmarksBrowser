2fabadd8bdaa24c887b202ae623b915d28c00d7b
salton <563185579@qq.com>
2019-02-19 10:34:19 +0800

    处理按钮动画

--- app/src/main/java/com/salton123/bookmarksbrowser/SplashActivity.java
+++ app/src/main/java/com/salton123/bookmarksbrowser/SplashActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 
 import com.salton123.base.BaseActivity;
 import com.salton123.base.feature.ImmersionFeature;
+import com.salton123.view.IconFontTextView;
 
 /**
  * User: newSalton@outlook.com
@@ -12,8 +13,9 @@ import com.salton123.base.feature.ImmersionFeature;
  * ModifyTime: 18:18
  * Description:
  */
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends BaseActivity implements View.OnClickListener {
     private ImmersionFeature mImmersionFeature;
+    private IconFontTextView tvActionLeft, tvActionRight, tvActionHome, tvActionWindows, tvActionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +23,37 @@ public class SplashActivity extends BaseActivity {
         addFeature(mImmersionFeature);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        findViewById(R.id.tvHint).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mImmersionFeature.dardFont();
-            }
-        });
+        initViewAndData();
+    }
+
+    private void initViewAndData() {
+        tvActionLeft = findViewById(R.id.tvActionLeft);
+        tvActionRight = findViewById(R.id.tvActionRight);
+        tvActionHome = findViewById(R.id.tvActionHome);
+        tvActionWindows = findViewById(R.id.tvActionWindows);
+        tvActionMenu = findViewById(R.id.tvActionMenu);
+        tvActionLeft.setOnClickListener(this);
+        tvActionRight.setOnClickListener(this);
+        tvActionHome.setOnClickListener(this);
+        tvActionWindows.setOnClickListener(this);
+        tvActionMenu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvActionLeft:
+                break;
+            case R.id.tvActionRight:
+                break;
+            case R.id.tvActionHome:
+                break;
+            case R.id.tvActionWindows:
+                break;
+            case R.id.tvActionMenu:
+                break;
+            default:
+                break;
+        }
     }
 }
